079246b457963156a689f350ab0e2d743231f225
anniemaggs <odonnellannie@hotmail.co.uk>
2018-04-19 10:51:46 +0000

    commiting dplyr-example2.R to staging area

---
+++ dplyr-example2.R
@@ -0,0 +1,11 @@
+
+## adding some new text to test the command line
+## annie annie annie
+
+library(downloader)
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/msleep_ggplot2.csv"
+filename <- "msleep_ggplot2.csv"
+if (!file.exists(filename)) download(url,filename)
+msleep <- read.csv("msleep_ggplot2.csv")
+head(msleep)
+
